4fd34d259e64bb28ea21a1b37300952bf892a051
YG Choi <yg.choi@dokahn.com>
2023-07-10 13:59:56 +0900

    fetching 을 이용한 CSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,19 +1,10 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
-import styles from "./page.module.css";
-
-// export const revalidate = 10; // ISR
+import MeowArticle from "@/components/MeowArticle";
 
 export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌.
   const products = await getProducts();
-  const res = await fetch("https://meowfacts.herokuapp.com", {
-    next: { revalidate: 3 },
-    // cache: "no-store",
-  }); // fetch 에서 revalidate 를 통한 ISR
-  const data = await res.json();
-  const factText = data.data[0];
-
   return (
     <>
       <h1>제품 설명 페이지!</h1>
@@ -24,7 +15,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
-      <article className={styles.article}>{factText}</article>
+      <MeowArticle />
     </>
   );
 }
---
+++ src/components/MeowArticle.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./MeowArticle.module.css";
+
+export default function MeowArticle() {
+  const [text, setText] = useState("데이터 준비 중...");
+
+  useEffect(() => {
+    fetch("https://meowfacts.herokuapp.com")
+      .then((res) => res.json())
+      .then((data) => setText(data.data[0]));
+  }, []);
+  return <article className={styles.article}>{text}</article>;
+}
